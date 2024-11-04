4da0f90a07de8fc9e52cd54ac6783471579f55d5
Vamsi <130114623+VamsiBunny@users.noreply.github.com>
2024-11-04 17:18:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-//complete this code
-class Animal {}
 
-class Dog extends Animal {}
+class Animal {
+	constructor(species){
+       this.species=species;
+	}
+ makeSound(){
+   console.log("The "+this.species+" makes a sound");
+ }
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	constructor(species){
+     super(species);
+	}
+ purr(){
+   console.log("purr");
+ }
+}
 
-// Do not change the code below this line
+class Dog extends Animal {
+	constructor(species){
+     super(species);
+	}
+    bark(){
+       console.log("woof");
+   }	
+}
+
+
 window.Animal = Animal;
 window.Dog = Dog;
 window.Cat = Cat;
